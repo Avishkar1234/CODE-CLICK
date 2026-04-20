6f240fa8e0ff97275bebccbd771ba0446ec7ff07
Avishkar <avishkar1234567890@gmail.com>
2026-04-20 21:30:50 +0530

    feat: update dependencies and integrate Clerk with Convex
    - Updated package.json to use stable versions of React and Next.js, added new dependencies for Clerk and Convex.
    - Refactored layout.tsx to include ClerkProvider and ConvexClientProvider for authentication and state management.
    - Simplified page.tsx to remove boilerplate and prepare for future content.
    - Added ConvexClientProvider component to manage Convex client with Clerk authentication.
    - Introduced middleware.ts to handle Clerk's authentication middleware for API routes and page requests.

---
+++ convex/http.ts
@@ -0,0 +1,67 @@
+import { httpRouter } from 'convex/server';
+import { httpAction } from './_generated/server';
+import process from 'process';
+import { WebhookEvent } from '@clerk/nextjs/server';
+import { Webhook } from 'svix';
+
+const http = httpRouter();
+
+http.route({
+  path: '/clerk-webhook',
+  method: 'POST',
+
+  handler: httpAction(async (ctx, request) => {
+    const webhookSecret = process.env.CLERK_WEBHOOK_SECRET;
+
+    if (!webhookSecret) {
+      throw new Error('Missing CLERK_WEBHOOK_SECRET envionment variable');
+    }
+
+    const svix_id = request.headers.get('svix_id');
+    const svix_signature = request.headers.get('svix_signature');
+    const svix_timestamp = request.headers.get('svix_timestamp');
+
+    if (!svix_id || !svix_signature || !svix_timestamp) {
+      return new Response('Error occurred -- no svix headers', {
+        status: 400,
+      });
+    }
+
+    const payload = await request.json();
+    const body = JSON.stringify(payload);
+
+    const wh = new Webhook(webhookSecret);
+    let evt: WebhookEvent;
+
+    try {
+      evt = wh.verify(body, {
+        'svix-id': svix_id,
+        'svix-timestamp': svix_timestamp,
+        'svix-signature': svix_signature,
+      }) as WebhookEvent;
+    } catch (err) {
+      console.error('Error verifying webhook', err);
+      return new Response('Error occurred', { status: 400 });
+    }
+
+    const eventType = evt.type;
+    if (eventType === 'user.created') {
+      // save the user to convex db
+
+      const { id, email_addresses, first_name, last_name } = evt.data;
+
+      const email = email_addresses[0].email_address;
+      const name = `${first_name || ''} ${last_name || ''}`.trim();
+
+      try {
+        // save user to db
+      } catch (error) {
+        return new Response('Error creating user', { status: 500 });
+      }
+    }
+
+    return new Response('Webhook processed successfully', { status: 200 });
+  }),
+});
+
+export default http;
